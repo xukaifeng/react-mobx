772be1bf17a7c8316ada695dbfe784ca9f6d6cf3
kaifeng <953555581@qq.com>
2020-10-14 19:14:48 +0800

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'antd';
-import { inject, observer } from '@global/mobx';
+import { inject, observer, observable, action } from '@global/mobx';
 import styles from './index.less';
 
 interface IProps {
@@ -13,22 +13,33 @@ interface IState {
 @inject('appState')
 @observer
 class Home extends React.Component<IProps, IState> {
+  @observable public t = 0;
+
   constructor(props: IProps) {
     super(props);
   }
   public render() {
     const { appState } = this.props;
     const { count, increase, decrease } = appState;
-    console.log(appState);
+    console.log(appState, this.t);
     return (
       <div>
-        <div>{count}</div>
+        <div>{this.props.appState.count}</div>
         <Button onClick={() => increase()} style={{ marginRight: 10 }}>
           increase
         </Button>
         <Button onClick={() => decrease()}>decrease</Button>
+        <div>-------------</div>
+        <div>{this.t}</div>
+        <Button onClick={() => this.add()}>t add</Button>
       </div>
     );
+  }
+
+  @action
+  public add() {
+    console.log(this.t);
+    this.t++;
   }
 }
 
